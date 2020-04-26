640d4303536ee4ebdbd5557b842ebf1177954718
Graham Patterson <gpatterson44@bloomberg.net>
2020-04-26 10:10:37 -0400

    Adds heroku calls.

--- picture_party_site/picture_party_site/settings.py
+++ picture_party_site/picture_party_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,3 +118,8 @@ STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "storage/")
 MEDIA_URL = "/storage/"
+
+
+# Keep at bottom of settings
+# Activate Django-Heroku.
+django_heroku.settings(locals())
